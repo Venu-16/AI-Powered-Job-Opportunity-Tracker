7770d24a5edf23623c28f1459a6c0ed9092ffba4
Venu-16 <venumadhav2212@gmail.com>
2025-12-18 22:22:30 +0530

    react app created fron frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>AI-Powered Job Opportunity Tracker</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
